eebf2b720ef6da7a70fafd6996d3a0e39f4fe71d
Tobias <tobb-on@hotmail.com>
2020-05-20 14:27:28 +0200

    small change

--- src/App.js
+++ src/App.js
@@ -10,29 +10,11 @@ import HeaderRedux from './components/HeaderRedux';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className='App'>
-      <header className='App-header'>
-        <h1> React installed </h1>
-      </header>
-      <main>
-        <SearchBar />
-        <Characters />
-        
-
-        {/* <Worlds />
-=======
     <div>
        <header> <HeaderRedux /></header>
->>>>>>> arthur
         <Worlds />
         <WorldsRedux />
-<<<<<<< HEAD
-        <WorldsRedux /> */}
-      </main>
-=======
       
->>>>>>> arthur
     </div>
   );
 }
