e91de2a30a4aa979f154a57b3a198e9d52baafb0
Tobias <tobb-on@hotmail.com>
2020-05-20 18:18:05 +0200

    updates appjs cssfiles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Worlds from './components/Worlds';
-import WorldsRedux from './components/WorldsRedux';
+import Home from './components/Home';
+import HeaderRedux from './components/HeaderRedux';
+import SearchBar from './components/SearchBar';
+import Characters from './components/Characters';
+import About from './components/About';
+import Hufvudstaden from './components/Hufvudstaden';
+import Slottsskogen from './components/Slottsskogen';
+import Donso from './components/Donso';
+import Hisingen from './components/Hisingen';
+
+
+
 
 function App() {
+
+  const HOME = 'Home', HUFVUDSTADEN = 'Hufvudstaden', SLOTTSSKOGEN = 'Slottsskogen',
+  DONSO = 'Donso', HISINGEN = 'Hisingen', CHARACTERS = 'Characters', ABOUT = 'About';
+
+  const [currentScreen, setCurrentScreen] = useState(HOME);
+
+  let content = null;
+  switch (currentScreen) {
+      case HOME:
+          content = ( <Home /> )
+          break;
+      case HUFVUDSTADEN:
+          content = ( <Hufvudstaden /> )
+          break;
+      case SLOTTSSKOGEN:
+          content = ( <Slottsskogen /> )
+          break;
+      case DONSO:
+            content = ( <Donso /> )
+            break;
+      case HISINGEN:
+            content = ( <Hisingen /> )
+            break;
+      case CHARACTERS:
+          content = ( <Characters /> )
+          break;
+      case ABOUT:
+          content = ( <About /> )
+          break;
+      default:
+          content = ( <Home /> )
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-          <h1> React installed </h1>
+      <header>
+      <HeaderRedux 
+        setScreenFromHeader={setCurrentScreen}
+      />
+
       </header>
       <main>
-        <Worlds />
-        <Worlds />
-        <WorldsRedux />
-        <WorldsRedux />
+        {content}
       </main>
     </div>
   );
