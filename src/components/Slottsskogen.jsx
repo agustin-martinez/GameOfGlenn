e56dac6dcea4c613128200f98765fca96df65201
Arthur Oresjö <arthur_jul@hotmail.com>
2020-05-21 16:58:14 +0200

    Fler fixar

--- src/components/Slottsskogen.jsx
+++ src/components/Slottsskogen.jsx
@@ -5,10 +5,20 @@ const Slottsskogen = () => {
 
     return (
         <div className="border">
-            <div>Slottsskogen</div>
+             <div>
+                
+                <h1 class="h1">Slottsskogen</h1>
                 <div>
                     <img src="Slottsskogen.svg" alt="Karta över Slottsskogen"></img>
                 </div>
+                <p class="p1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga, est quae. Fuga reprehenderit facere, sint quis laborum consequuntur atque aperiam vel quaerat eos, quas itaque eius. Delectus consectetur dolore suscipit eligendi quidem quibusdam porro nulla voluptate quaerat reprehenderit consequuntur sit, pariatur voluptatem necessitatibus rem provident vel tenetur impedit vitae libero a repudiandae culpa. Eius eaque velit ad dolorum. Ab distinctio et commodi obcaecati totam amet nesciunt accusamus quia itaque, officia neque pariatur, dolore suscipit unde eum quas. Alias, ad a? Voluptatibus qui architecto adipisci nulla inventore eius, earum delectus at. Est exercitationem blanditiis iusto unde quia, a accusamus id repellendus?</p>
+                
+                    <h2 class="h2">Karaktärer som är här</h2>
+                
+                <div>
+                   
+                </div>
+            </div>
             
         </div>
     )
