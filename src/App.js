c5e0b08f200a33447e7135156550287eaa544c16
Alexander Caspersson <alexander.casperssonrgmail.com>
2020-05-24 15:50:04 +0200

    added searchbar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import './App.css';
 import Home from './components/Home';
 import HeaderRedux from './components/HeaderRedux';
-import SearchBar from './components/SearchBar';
 import Characters from './components/Characters';
 import About from './components/About';
 import Hufvudstaden from './components/Hufvudstaden';
@@ -10,54 +9,50 @@ import Slottsskogen from './components/Slottsskogen';
 import Donso from './components/Donso';
 import Hisingen from './components/Hisingen';
 
-
-
-
 function App() {
-
-  const HOME = 'Home', HUFVUDSTADEN = 'Hufvudstaden', SLOTTSSKOGEN = 'Slottsskogen',
-  DONSO = 'Donso', HISINGEN = 'Hisingen', CHARACTERS = 'Characters', ABOUT = 'About';
+  const HOME = 'Home',
+    HUFVUDSTADEN = 'Hufvudstaden',
+    SLOTTSSKOGEN = 'Slottsskogen',
+    DONSO = 'Donso',
+    HISINGEN = 'Hisingen',
+    CHARACTERS = 'Characters',
+    ABOUT = 'About';
 
   const [currentScreen, setCurrentScreen] = useState(HOME);
 
   let content = null;
   switch (currentScreen) {
-      case HOME:
-          content = ( <Home /> )
-          break;
-      case HUFVUDSTADEN:
-          content = ( <Hufvudstaden /> )
-          break;
-      case SLOTTSSKOGEN:
-          content = ( <Slottsskogen /> )
-          break;
-      case DONSO:
-            content = ( <Donso /> )
-            break;
-      case HISINGEN:
-            content = ( <Hisingen /> )
-            break;
-      case CHARACTERS:
-          content = ( <Characters /> )
-          break;
-      case ABOUT:
-          content = ( <About /> )
-          break;
-      default:
-          content = ( <Home /> )
-}
+    case HOME:
+      content = <Home />;
+      break;
+    case HUFVUDSTADEN:
+      content = <Hufvudstaden />;
+      break;
+    case SLOTTSSKOGEN:
+      content = <Slottsskogen />;
+      break;
+    case DONSO:
+      content = <Donso />;
+      break;
+    case HISINGEN:
+      content = <Hisingen />;
+      break;
+    case CHARACTERS:
+      content = <Characters />;
+      break;
+    case ABOUT:
+      content = <About />;
+      break;
+    default:
+      content = <Home />;
+  }
 
   return (
-    <div className="App">
+    <div className='App'>
       <header>
-      <HeaderRedux 
-        setScreenFromHeader={setCurrentScreen}
-      />
-
+        <HeaderRedux setScreenFromHeader={setCurrentScreen} />
       </header>
-      <main>
-        {content}
-      </main>
+      <main>{content}</main>
     </div>
   );
 }
--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,52 +1,45 @@
 import React, { useState } from 'react';
-import AddCharacter from '../components/AddCharacter';
+import AddCharacter from './AddCharacter';
+import SearchBar from './SearchBar';
 import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux'
-import { actions } from '../features/characters'
+import { useDispatch } from 'react-redux';
+import { actions } from '../features/characters';
 import '../popup.css';
 
-
 const Characters = () => {
-    const dispatch = useDispatch();
-    const charactersData = useSelector(state => state.characters)
-    const [showAdd, setShowAdd] = useState(false);
-    
-    let maybeAdd = null;
-    if( showAdd ) {
-        maybeAdd = <AddCharacter setShowAddChild={setShowAdd}/> ;
-    }
+  const dispatch = useDispatch();
+  const charactersData = useSelector((state) => state.characters);
+  const [showAdd, setShowAdd] = useState(false);
 
-    
-const SearchBar = (
-  <div  className='SearchBar' style={{ marginBottom: '30px' }}>
-    <input
-      type='text'
-      placeholder='Sök efter karaktärer ...'
-      name='sök karktärer'
-      ></input>
+  let maybeAdd = null;
+  if (showAdd) {
+    maybeAdd = <AddCharacter setShowAddChild={setShowAdd} />;
+  }
+
+  const outputCharacters = charactersData.map((charItem) => (
+    <div className='planets-item' key={charItem.character.name}>
+      <div> NAME: {charItem.character.name} </div>
+      <div> WORLD {charItem.character.world} </div>
+      <div> STRENGHT: {charItem.strength} </div>
+      <button
+        onClick={() =>
+          dispatch(actions.removeCharacter(charItem.character.name))
+        }
+      >
+        Ta bort
+      </button>
     </div>
-  )
-
-    const outputCharacters = charactersData.map(charItem => (
-        <div className="planets-item" key={charItem.character.name}>
-            <div> NAME: {charItem.character.name} </div>
-            <div> WORLD {charItem.character.world} </div>
-            <div> STRENGHT: {charItem.strength} </div>
-            <button  onClick={() => dispatch(actions.removeCharacter(charItem.character.name))}>Ta bort</button>
-        </div>
-    ))
-    return (
-        <div className="border">
-            
-            {SearchBar}
-            <div className="container">
-            <div className="persons">{outputCharacters}</div>
-            </div>
-            <button onClick={() => setShowAdd(!showAdd)}>Toggle</button>
-            {maybeAdd}
-            
-        </div>
-    )
-}
+  ));
+  return (
+    <div className='border'>
+      <SearchBar />
+      <div className='container'>
+        <div className='persons'>{outputCharacters}</div>
+      </div>
+      <button onClick={() => setShowAdd(!showAdd)}>Toggle</button>
+      {maybeAdd}
+    </div>
+  );
+};
 
 export default Characters;
