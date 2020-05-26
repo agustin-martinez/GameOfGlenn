de3332d6cef0960544fe714856148c2d74bd8a03
Sandra Dahlgren <sandradahlgren@Sandras-MacBook.local>
2020-05-26 18:56:23 +0200

    changing-other-components

--- src/components/Donso.jsx
+++ src/components/Donso.jsx
@@ -1,51 +1,54 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 
 const Donso = () => {
-    const charactersData = useSelector(state => state.characters)
-	const [fromDonso, setFromDonso] = useState([])
+  const charactersData = useSelector((state) => state.characters);
+  const [fromDonso, setFromDonso] = useState([]);
 
-    useEffect(() => {
-        const results = charactersData.filter(item =>
-        item.world.includes('Donsö')
-        );
-        setFromDonso(results);
-    }, [charactersData]);
+  useEffect(() => {
+    const results = charactersData.filter((item) =>
+      item.world.includes("Donsö")
+    );
+    setFromDonso(results);
+  }, [charactersData]);
 
-    const outputCharacters = fromDonso.map(charItem => (
-        <div className="Characters" key={charItem.name}>
-            <div> NAMN: {charItem.name} </div>
-            <div> VÄRLD: {charItem.world} </div>
-            <div> INTELLIGENS: {charItem.intelligence} </div>
-            <div> STYRKA: {charItem.strength} </div>
-            <div> BACKSTORY: {charItem.backstory} </div>
-            <div> ID: {charItem.id} </div>
-            {/* <button  onClick={() => deleteCharacter(charactersItem)}>Delete</button> */}
+  const outputCharacters = fromDonso.map((charItem) => (
+    <div className="Characters" key={charItem.name}>
+      <div> NAMN: {charItem.name} </div>
+      <div> VÄRLD: {charItem.world} </div>
+      <div> INTELLIGENS: {charItem.intelligence} </div>
+      <div> STYRKA: {charItem.strength} </div>
+      <div> BACKSTORY: {charItem.backstory} </div>
+      <div> ID: {charItem.id} </div>
+      {/* <button  onClick={() => deleteCharacter(charactersItem)}>Delete</button> */}
+    </div>
+  ));
+
+  return (
+    <div className="border">
+      <div>
+        <h1 className="h1">Donso</h1>
+        <div>
+          <img src="Donso.svg" alt="Karta över Donso"></img>
         </div>
-    ))
-
-    return (
-        <div className="border">
-			<div>
-				<h1 className="h1">
-					Donso
-				</h1>
-				<div>
-					<img src="Donso.svg" alt="Karta över Donso">
-					</img>
-				</div>
-				<p className="p1">
-				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga, est quae. Fuga reprehenderit facere, sint quis laborum consequuntur atque aperiam vel quaerat eos, quas itaque eius. Delectus consectetur dolore suscipit eligendi quidem quibusdam porro nulla voluptate quaerat reprehenderit consequuntur sit, pariatur voluptatem necessitatibus rem provident vel tenetur impedit vitae libero a repudiandae culpa. Eius eaque velit ad dolorum. Ab distinctio et commodi obcaecati totam amet nesciunt accusamus quia itaque, officia neque pariatur, dolore suscipit unde eum quas. Alias, ad a? Voluptatibus qui architecto adipisci nulla inventore eius, earum delectus at. Est exercitationem blanditiis iusto unde quia, a accusamus id repellendus?
-				</p>
-				<h2 className="h2">
-				Karaktärer som är här
-				</h2>
-				<div>
-				{outputCharacters}
-				</div>
-			</div>
-        </div>
-    )
-}
+        <p className="Text">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga, est
+          quae. Fuga reprehenderit facere, sint quis laborum consequuntur atque
+          aperiam vel quaerat eos, quas itaque eius. Delectus consectetur dolore
+          suscipit eligendi quidem quibusdam porro nulla voluptate quaerat
+          reprehenderit consequuntur sit, pariatur voluptatem necessitatibus rem
+          provident vel tenetur impedit vitae libero a repudiandae culpa. Eius
+          eaque velit ad dolorum. Ab distinctio et commodi obcaecati totam amet
+          nesciunt accusamus quia itaque, officia neque pariatur, dolore
+          suscipit unde eum quas. Alias, ad a? Voluptatibus qui architecto
+          adipisci nulla inventore eius, earum delectus at. Est exercitationem
+          blanditiis iusto unde quia, a accusamus id repellendus?
+        </p>
+        <h2 className="h2">Karaktärer som är här</h2>
+        <div>{outputCharacters}</div>
+      </div>
+    </div>
+  );
+};
 
 export default Donso;
--- src/components/Hufvudstaden.jsx
+++ src/components/Hufvudstaden.jsx
@@ -1,51 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 
 const Hufvudstaden = () => {
-    const charactersData = useSelector(state => state.characters)
-    const [fromHufvudstaden, setFromHufvudstaden] = useState([])
+  const charactersData = useSelector((state) => state.characters);
+  const [fromHufvudstaden, setFromHufvudstaden] = useState([]);
 
-    useEffect(() => {
-        const results = charactersData.filter(item =>
-        item.world.includes('Hufvudstaden')
-        );
-        setFromHufvudstaden(results);
-    }, [charactersData]);
+  useEffect(() => {
+    const results = charactersData.filter((item) =>
+      item.world.includes("Hufvudstaden")
+    );
+    setFromHufvudstaden(results);
+  }, [charactersData]);
 
-    const outputCharacters = fromHufvudstaden.map(charItem => (
-        <div className="Characters" key={charItem.name}>
-            <div> NAMN: {charItem.name} </div>
-            <div> VÄRLD: {charItem.world} </div>
-            <div> INTELLIGENS: {charItem.intelligence} </div>
-            <div> STYRKA: {charItem.strength} </div>
-            <div> BACKSTORY: {charItem.backstory} </div>
-            <div> ID: {charItem.id} </div>
-            {/* <button  onClick={() => deleteCharacter(charactersItem)}>Delete</button> */}
+  const outputCharacters = fromHufvudstaden.map((charItem) => (
+    <div className="Characters" key={charItem.name}>
+      <div> NAMN: {charItem.name} </div>
+      <div> VÄRLD: {charItem.world} </div>
+      <div> INTELLIGENS: {charItem.intelligence} </div>
+      <div> STYRKA: {charItem.strength} </div>
+      <div> BACKSTORY: {charItem.backstory} </div>
+      <div> ID: {charItem.id} </div>
+      {/* <button  onClick={() => deleteCharacter(charactersItem)}>Delete</button> */}
+    </div>
+  ));
+
+  return (
+    <div className="border">
+      <div>
+        <h1 className="h1">Hufvudstaden</h1>
+        <div>
+          <img src="Hufvudstaden.svg" alt="Karta över Hufvudstaden"></img>
         </div>
-    ))
+        <p className="Text">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga, est
+          quae. Fuga reprehenderit facere, sint quis laborum consequuntur atque
+          aperiam vel quaerat eos, quas itaque eius. Delectus consectetur dolore
+          suscipit eligendi quidem quibusdam porro nulla voluptate quaerat
+          reprehenderit consequuntur sit, pariatur voluptatem necessitatibus rem
+          provident vel tenetur impedit vitae libero a repudiandae culpa. Eius
+          eaque velit ad dolorum. Ab distinctio et commodi obcaecati totam amet
+          nesciunt accusamus quia itaque, officia neque pariatur, dolore
+          suscipit unde eum quas. Alias, ad a? Voluptatibus qui architecto
+          adipisci nulla inventore eius, earum delectus at. Est exercitationem
+          blanditiis iusto unde quia, a accusamus id repellendus?
+        </p>
 
-    return (
-        <div className="border">
-            <div>
-                
-                <h1 className="h1">Hufvudstaden</h1>
-                <div>
-                    <img src="Hufvudstaden.svg" alt="Karta över Hufvudstaden"></img>
-                </div>
-                <p className="p1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga, est quae. Fuga reprehenderit facere, sint quis laborum consequuntur atque aperiam vel quaerat eos, quas itaque eius. Delectus consectetur dolore suscipit eligendi quidem quibusdam porro nulla voluptate quaerat reprehenderit consequuntur sit, pariatur voluptatem necessitatibus rem provident vel tenetur impedit vitae libero a repudiandae culpa. Eius eaque velit ad dolorum. Ab distinctio et commodi obcaecati totam amet nesciunt accusamus quia itaque, officia neque pariatur, dolore suscipit unde eum quas. Alias, ad a? Voluptatibus qui architecto adipisci nulla inventore eius, earum delectus at. Est exercitationem blanditiis iusto unde quia, a accusamus id repellendus?</p>
-                
-                    <h2 className="h2">Karaktärer som är här</h2>
-                
-                <div>
-                {outputCharacters}
-                </div>
-            </div>
-                
-        </div>
-        
-    )
-    
-       
-}
+        <h2 className="h2">Karaktärer som är här</h2>
+
+        <div>{outputCharacters}</div>
+      </div>
+    </div>
+  );
+};
 
 export default Hufvudstaden;
