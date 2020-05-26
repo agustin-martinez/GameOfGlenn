2a2e4e3db5eb74741e0ff9992fc586147658734a
agustin-martinez <agustin.martinez@hotmail.com>
2020-05-26 14:42:20 +0200

    cards-and-more

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,95 +1,122 @@
-import React, { useState, useEffect } from 'react';
-import AddCharacter from '../components/AddCharacter';
-import EditCharacter from '../components/EditCharacter';
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux'
-import { actions } from '../features/characters'
-import { actions as actionsEdit } from '../features/editCharacter'
-import '../popup.css';
-
+import React, { useState, useEffect } from "react";
+import AddCharacter from "../components/AddCharacter";
+import EditCharacter from "../components/EditCharacter";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { actions } from "../features/characters";
+import { actions as actionsEdit } from "../features/editCharacter";
+import "../popup.css";
 
 const Characters = () => {
-    const dispatch = useDispatch();
-    const charactersData = useSelector(state => state.characters)
-    const [showAdd, setShowAdd] = useState(false);
-    const [showEdit, setShowEdit] = useState(false);
+  const dispatch = useDispatch();
+  const charactersData = useSelector((state) => state.characters);
+  const [showAdd, setShowAdd] = useState(false);
+  const [showEdit, setShowEdit] = useState(false);
 
-    //---------------
-    const [searchTerm, setSearchTerm] = useState('');
-    const [resultList, setResultList] = useState(charactersData);
+  //---------------
+  const [searchTerm, setSearchTerm] = useState("");
+  const [resultList, setResultList] = useState(charactersData);
 
-    useEffect(() => {
-        setResultList(charactersData);
-    }, [charactersData]);
+  useEffect(() => {
+    setResultList(charactersData);
+  }, [charactersData]);
 
-    const handleChange = (e) => {
-        console.log(charactersData);
-        setSearchTerm(e.target.value);
-        setResultList(
-          charactersData.filter((person) => 
+  const handleChange = (e) => {
+    console.log(charactersData);
+    setSearchTerm(e.target.value);
+    setResultList(
+      charactersData.filter(
+        (person) =>
           person.name.toLowerCase().includes(e.target.value) ||
           person.name.toUpperCase().includes(e.target.value)
-          )
-        );
-      };
-      //-------------------
-    
-    let maybeAdd = null;
-    if( showAdd ) {
-        maybeAdd = <AddCharacter setShowAddChild={setShowAdd}/> ;
-    }
+      )
+    );
+  };
+  //-------------------
 
-    let maybeEdit = null;
-    if( showEdit ) {
-        maybeEdit = <EditCharacter setShowAddChild={setShowEdit}/> ;
-    }
-    
-    const handleEdit = (char) => {
-        dispatch(actionsEdit.addToEditForm(char))
+  let maybeAdd = null;
+  if (showAdd) {
+    maybeAdd = <AddCharacter setShowAddChild={setShowAdd} />;
+  }
 
-        setShowEdit(!showEdit)
-        
-      };
-     
-    
-    
-const SearchBar = (
-    <div  className='SearchBar' style={{ marginBottom: '30px' }}>
-    <input
-        type='text'
-        placeholder='Sök efter karaktärer ...'
-        name='sök karktärer'
+  let maybeEdit = null;
+  if (showEdit) {
+    maybeEdit = <EditCharacter setShowAddChild={setShowEdit} />;
+  }
+
+  const handleEdit = (char) => {
+    dispatch(actionsEdit.addToEditForm(char));
+
+    setShowEdit(!showEdit);
+  };
+
+  const SearchBar = (
+    <div className="SearchBar" style={{ marginBottom: "30px" }}>
+      <input
+        className="character-input"
+        type="text"
+        placeholder="Sök efter karaktärer ..."
+        name="sök karktärer"
         value={searchTerm}
         onChange={handleChange}
-        ></input>
+      ></input>
     </div>
-    )
+  );
 
-    const outputCharacters = resultList.map(charItem => (
-        <div className="character-item" key={charItem.name}>
-            <div> NAMN: {charItem.name} </div>
-            <div> VÄRLD: {charItem.world} </div>
-            <div> INTELLIGENS: {charItem.intelligence} </div>
-            <div> STYRKA: {charItem.strength} </div>
-            <div> BACKSTORY: {charItem.backstory} </div>
-            <div> ID: {charItem.id} </div>
-            <button  onClick={() => dispatch(actions.removeCharacter(charItem.name))}>Ta bort</button>
-            <button onClick={() => handleEdit(charItem)}>Edit Character</button>
-        </div>
-    ))
-    return (
-        <div className="border">
-            
-            {SearchBar}
-            <div className="container">
-            <div className="persons">{outputCharacters}</div>
+  const outputCharacters = resultList.map((charItem) => (
+    <div className="character-item" key={charItem.name}>
+      <div className="card-content">
+        <div className="name-box">
+          <div className="name"> {charItem.name} </div>
+          <div className="btm-box">
+            <div className="btm-edit-box">
+              <button
+                className="btm-edit"
+                onClick={() => handleEdit(charItem)}
+              ></button>
             </div>
-            <button onClick={() => setShowAdd(!showAdd)}>Toggle</button>
-            {maybeAdd}
-            {maybeEdit}
-            
+            <div className="btm-delete-box">
+              <button
+                className="btm-delete"
+                onClick={() => dispatch(actions.removeCharacter(charItem.name))}
+              ></button>
+            </div>
+          </div>
         </div>
-    )
-}
+        <div className="world">
+          <div className="world-text"> VÄRLD: {charItem.world} </div>
+          <div className="world-pic"> {charItem.pic} </div>
+        </div>
+        <div className="backstory"> BACKSTORY: {charItem.backstory} </div>
+        <div className="iq">INTELLIGENS:</div>
+        <input
+          readOnly
+          type="range"
+          max="10"
+          value={charItem.intelligence}
+        ></input>
+        <div className="power"> STYRKA: </div>
+        <input readOnly type="range" max="10" value={charItem.strength}></input>
+        <div className="id"> ID: {charItem.id} </div>
+      </div>
+    </div>
+  ));
+  return (
+    <div className="border">
+      {SearchBar}
+      <button
+        className="btm-add-character"
+        onClick={() => setShowAdd(!showAdd)}
+      >
+        Add character
+      </button>
+      <div className="container">
+        <div className="persons">{outputCharacters}</div>
+      </div>
+      {maybeAdd}
+      {maybeEdit}
+    </div>
+  );
+};
 
 export default Characters;
--- src/features/characters.js
+++ src/features/characters.js
@@ -1,47 +1,86 @@
-import { createAction, createReducer } from '@reduxjs/toolkit';
+import { createAction, createReducer } from "@reduxjs/toolkit";
+import { slotts } from "../components/assets/mini-slottskogen.svg";
+import { hufvudstaden } from "../components/assets/mini-slottskogen.svg";
+import { donsö } from "../components/assets/mini-slottskogen.svg";
+import { hisingen } from "../components/assets/mini-slottskogen.svg";
 
-const addToCharacters = createAction('add character to list');
-const removeCharacter = createAction('remove from characters');
-const editCharacter = createAction('edit character');
-const increaseStrength = createAction('increase Strenth')
-const decreaseStrength = createAction('decrease Strenth')
+const addToCharacters = createAction("add character to list");
+const removeCharacter = createAction("remove from characters");
+const editCharacter = createAction("edit character");
+const increaseStrength = createAction("increase Strenth");
+const decreaseStrength = createAction("decrease Strenth");
 
 // const alredyAdded = createAction('if alredy added');
 
-const actions = { addToCharacters, removeCharacter, editCharacter, increaseStrength, decreaseStrength };
+const actions = {
+  addToCharacters,
+  removeCharacter,
+  editCharacter,
+  increaseStrength,
+  decreaseStrength,
+};
 
 const initialState = [
-    {id: 1, name: 'Ragnhild', world: 'Hufvudstaden', backstory: 'fdffd', strength: 6, intelligence: 5 },
-    {id: 2, name: 'Bo', world: 'Slottskogen', backstory: 'dsd', strength: 4, intelligence: 4 },
-    {id: 3, name: 'Lars', world: 'Donsö', backstory: 'vds', strength: 3, intelligence: 6 },
-    {id: 4, name: 'Karl', world: 'Hisingen', backstory: 'fdffd', strength: 5, intelligence: 7 }
-]
-
+  {
+    id: 1,
+    name: "RAGNHILD",
+    world: "Hufvudstaden",
+    pic: hufvudstaden,
+    backstory:
+      "Gandalf är enbart hobernas och nordmännens namn på honom. Alverna känner honom som Mithrandir Grå vandrare och även männen av Gondor. ",
+    strength: 6,
+    intelligence: 5,
+  },
+  {
+    id: 2,
+    name: "BO",
+    world: "Slottskogen",
+    pic: slotts,
+    backstory:
+      "Gandalf är enbart hobernas och nordmännens namn på honom. Alverna känner honom som Mithrandir Grå vandrare och även männen av Gondor. ",
+    strength: 4,
+    intelligence: 4,
+  },
+  {
+    id: 3,
+    name: "LARS",
+    world: "Donsö",
+    pic: donsö,
+    backstory:
+      "Gandalf är enbart hobernas och nordmännens namn på honom. Alverna känner honom som Mithrandir Grå vandrare och även männen av Gondor. ",
+    strength: 3,
+    intelligence: 6,
+  },
+  {
+    id: 4,
+    name: "KARL",
+    world: "Hisingen",
+    pic: hisingen,
+    backstory:
+      "Gandalf är enbart hobernas och nordmännens namn på honom. Alverna känner honom som Mithrandir Grå vandrare och även männen av Gondor. ",
+    strength: 5,
+    intelligence: 7,
+  },
+];
 
 const reducer = createReducer(initialState, {
-    [addToCharacters]: (state, action) => {
-            return [
-                ...state,
-                action.payload
-            ];
-        
-    },  // addToCart
+  [addToCharacters]: (state, action) => {
+    return [...state, action.payload];
+  }, // addToCart
 
-    [removeCharacter]: (state, action) => (
-        state.filter(cartItem => cartItem.name !== action.payload)
-    ),
+  [removeCharacter]: (state, action) =>
+    state.filter((cartItem) => cartItem.name !== action.payload),
 
-    [editCharacter]: (state, action) => state.map((character, index) => {
-      if( character.id === action.payload.id ) {
-          return action.payload;
+  [editCharacter]: (state, action) =>
+    state.map((character, index) => {
+      if (character.id === action.payload.id) {
+        return action.payload;
       } else {
-          return character
+        return character;
       }
       // Alternativ: return (character.id === action.payload.id) ? action.payload : character
-  })
-
-    
-})
+    }),
+});
 
 // [editCharacter]: (state, action) => {
 //   return state.map((item, index) => {
@@ -58,12 +97,12 @@ const reducer = createReducer(initialState, {
 // },
 
 // function alreadyAdded () {
-    
+
 // }
 
 // const reducer = createReducer(initialState, {
 //     [addToCharacters]: (state, action) => {
-//         let found = state.find(characterItem => characterItem.character.name === 
+//         let found = state.find(characterItem => characterItem.character.name ===
 //         action.payload.name);
 //         if(found) {
 //             alreadyAdded(action.payload.name)
@@ -80,7 +119,7 @@ const reducer = createReducer(initialState, {
 //     state.map((cartItem ,index) => {
 //         if( index !== action.index ) {
 //             return cartItem
-//         } 
+//         }
 //             return {
 //             ...cartItem,
 //             ...action.cartItem
@@ -88,8 +127,4 @@ const reducer = createReducer(initialState, {
 //     })
 // )
 
-
-
-
-
-export { actions, reducer }
+export { actions, reducer };
