cb66df7d84812e76712c3098f29f7d50a093cf01
Tobias <tobb-on@hotmail.com>
2020-05-28 12:16:11 +0200

    Updates

---
+++ precache-manifest.a4522bcf726e8119132ebe64f89c6123.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c8ba32cba01be0169deb32732e3039",
+    "url": "/GameOfGlenn/index.html"
+  },
+  {
+    "revision": "eb1fca791aa184ca641c",
+    "url": "/GameOfGlenn/static/css/main.68d0e447.chunk.css"
+  },
+  {
+    "revision": "ddbe3950b2adbe709bff",
+    "url": "/GameOfGlenn/static/js/2.197b9fd8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GameOfGlenn/static/js/2.197b9fd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb1fca791aa184ca641c",
+    "url": "/GameOfGlenn/static/js/main.16d3c729.chunk.js"
+  },
+  {
+    "revision": "87fa47b198386d9e28f1",
+    "url": "/GameOfGlenn/static/js/runtime-main.36c8aca0.js"
+  },
+  {
+    "revision": "f5acbc74b32cf0370891e20a86cee89f",
+    "url": "/GameOfGlenn/static/media/GOG-Banner.f5acbc74.svg"
+  },
+  {
+    "revision": "229c85beb8b341958b109d5d66e1f572",
+    "url": "/GameOfGlenn/static/media/Slottsskogen-big-in-assets.229c85be.svg"
+  },
+  {
+    "revision": "7f2995af08e23edf7d581ca8fb758709",
+    "url": "/GameOfGlenn/static/media/aboutGOG.7f2995af.svg"
+  },
+  {
+    "revision": "e0863a20ef02aadabe9acec4ec226bb1",
+    "url": "/GameOfGlenn/static/media/aboutGOGmedia.e0863a20.svg"
+  },
+  {
+    "revision": "426eb08bd82f1ff7448f5a8baae9134c",
+    "url": "/GameOfGlenn/static/media/banner1.426eb08b.svg"
+  },
+  {
+    "revision": "e7e810e53815fe299d425c570213e80a",
+    "url": "/GameOfGlenn/static/media/banner2.e7e810e5.svg"
+  },
+  {
+    "revision": "2cf305c2a3070246012dbf3c24f596e3",
+    "url": "/GameOfGlenn/static/media/bannerInfo.2cf305c2.svg"
+  },
+  {
+    "revision": "2bfdfb260a7ccb8a5e6e5516016dc7f8",
+    "url": "/GameOfGlenn/static/media/bannerMedia.2bfdfb26.svg"
+  },
+  {
+    "revision": "25db2c258453bec9e3047b3ea1061d8d",
+    "url": "/GameOfGlenn/static/media/character-card-frame.25db2c25.svg"
+  },
+  {
+    "revision": "13aaf573bafe353ea1c921a5f1763f24",
+    "url": "/GameOfGlenn/static/media/cool-menu.13aaf573.svg"
+  },
+  {
+    "revision": "e2830a9ad21b01ad90d342c2c61b6051",
+    "url": "/GameOfGlenn/static/media/edit.e2830a9a.svg"
+  },
+  {
+    "revision": "fb0187fd9adaed83cefd5c5cca88e327",
+    "url": "/GameOfGlenn/static/media/glenn.fb0187fd.png"
+  },
+  {
+    "revision": "cf234808dd86a3f2f31081e43fcfc974",
+    "url": "/GameOfGlenn/static/media/logo.cf234808.svg"
+  },
+  {
+    "revision": "ed86f17b872a3d4b386ae050d0417072",
+    "url": "/GameOfGlenn/static/media/map-big.ed86f17b.png"
+  },
+  {
+    "revision": "f245aa6751fb210bad6a4f279fe90277",
+    "url": "/GameOfGlenn/static/media/mini-glenn.f245aa67.png"
+  },
+  {
+    "revision": "71c5e18edd7e292ccc0c7fcf3b750009",
+    "url": "/GameOfGlenn/static/media/mini-slottskogen.71c5e18e.svg"
+  },
+  {
+    "revision": "2d0aa98416c59b2a18102b1673a24f9b",
+    "url": "/GameOfGlenn/static/media/skull-delete.2d0aa984.svg"
+  },
+  {
+    "revision": "14d379b22b6fad1757c25b80cce1afb4",
+    "url": "/GameOfGlenn/static/media/snake.14d379b2.svg"
+  }
+]);
